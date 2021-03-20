26841509616040528e834378e60e6ff2e227d048
cYpHeR <github.com/cypher-nullbyte>
2021-03-21 04:52:33 +0530

    Typing Sycn done

--- server/public/chat.js
+++ server/public/chat.js
@@ -6,7 +6,8 @@ var socket = io.connect('http://localhost:4000');
 var message = document.getElementById('message'),
       handle = document.getElementById('handle'),
       btn = document.getElementById('send'),
-      output = document.getElementById('output');
+      output = document.getElementById('output'),
+      feedback=document.getElementById('feedback');
 
 // Emit events
 btn.addEventListener('click', function(){
@@ -17,7 +18,16 @@ btn.addEventListener('click', function(){
   message.value = "";
 });
 
+message.addEventListener('keypress',()=>{
+    socket.emit('typing',handle.value);
+})
+
+
 // Listen for events
 socket.on('chat', function(data){
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing',(data)=>{
+    feedback.innerHTML='<p><em>'+data+'is typing ...</em></p>';
 });
--- server/server.js
+++ server/server.js
@@ -24,6 +24,11 @@ io.on('connection',(socket)=>{
     socket.on('chat',function(data){
         io.sockets.emit('chat',data);
     });
+
+    socket.on('typing',function(data)
+    {
+        socket.broadcast.emit('typing',data);
+    });
 });
 
 
